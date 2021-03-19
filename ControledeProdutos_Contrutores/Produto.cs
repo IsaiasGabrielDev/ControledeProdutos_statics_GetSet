e19b7161569490df2aa7d2ce6e81d3476709d3b1
IsaiasGabriel <isaiasgabrielhub@gmail.com>
2021-03-19 18:11:27 -0300

    first commit

---
+++ ControledeProdutos_Contrutores/Produto.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ControledeProdutos_Contrutores
+{
+    class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto() //Serve para istanciar mesmo sem passar nenhum parametro
+        { }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco) //sobrecarga de construtor
+        {
+            _nome = nome;
+            _preco = preco;
+            // Quantidade = 0; Opcional
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1) //filtro de entrada de dados
+            {
+                _nome = nome;
+            }
+        }
+
+        public double Getpreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantd()
+        {
+            return _quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + _quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ControledeProdutos_Contrutores/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ControledeProdutos_Contrutores
+{
+    class Program
+    {
+        static void Main(string[] args) //Utilizando Sobrecarga em construtores de objetos.
+        {
+            
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Produto p = new Produto(nome, preco); //ele aceita tanto os 3 parametros como apenas 2.
+
+            /* // Syntasse alternativa, independente de não haver construtores.
+             Produto p = new Produto 
+            {   
+                Nome = "TV",
+                Preco = 500.00,
+                Quantidade = 20
+            };
+
+             */
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
